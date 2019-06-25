41a8e45a99ea6875613e99e15a633015b73f59b3
drmcastles <drmcast@yandex.ru>
2019-06-25 06:02:08 +0300

    commit

--- triangle.rb
+++ triangle.rb
@@ -7,16 +7,17 @@ sideC = gets.chomp.to_f
 if sideA**2 +  sideB**2 == sideC**2 or
    sideA**2 +  sideC**2 == sideB**2 or
    sideB**2 +  sideC**2 == sideA**2 
-   puts 'Прямоугольный треугольник' 
+   puts 'Этот треугольник прямоугольный' 
 
 if sideA == sideB or
-	sideB == sideC or
-	sideA == sideC 
-    puts 'Прямоугольный и Равнобедренный треугольник'
+   sideB == sideC or
+   sideA == sideC 
+   puts 'Этот треугольник прямоугольный и равнобедренный треугольник'
 end
 
 elsif sideA == sideB and
 	  sideA == sideC 
-	  puts 'Равносторонний и равнобедренный, но не прямоугольный'
-else puts 'Треугольник не является прямоугольным или равносторонним'	
+	  puts 'Этот треугольник равносторонний и равнобедренный, но не прямоугольный'
+else 
+	  puts 'Треугольник не является прямоугольным или равносторонним'	
 end
