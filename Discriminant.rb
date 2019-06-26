8f521c988008cc01b88e5794912dac9fbd3e5944
drmcastles <drmcast@yandex.ru>
2019-06-26 10:07:15 +0300

    last commit

--- Discriminant.rb
+++ Discriminant.rb
@@ -5,15 +5,15 @@ b = gets.chomp.to_f
 puts 'Введите c: '
 c = gets.chomp.to_f
 
-D = b**2 -4 * a * c
+D = b ** 2 - 4 * a * c
 sqrt_D = Math.sqrt(D)
 
 if D > 0
-  x1 = -b + sqrt_D / 2 * a
-  x2 = -b - sqrt_D / 2 * a
+  x1 = - b + sqrt_D / 2 * a
+  x2 = - b - sqrt_D / 2 * a
   puts " D = #{D}\n x1 = #{x1}\n x2= #{x2}"
 elsif D == 0
-  x1 = -b + sqrt_D / 2 * a
+  x1 = - b + sqrt_D / 2 * a
   puts " D = #{D}\n x1 = #{x1}"
 else
   puts " D = #{D}"
