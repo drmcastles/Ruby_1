29a392cd9a4633fd3494452606c467784ce30c65
drmcastles <drmcast@yandex.ru>
2019-06-26 13:17:47 +0300

    last commit

--- weight.rb
+++ weight.rb
@@ -2,5 +2,5 @@
 name = gets.chomp
 puts 'Каков ваш рост в сантиметрах?'
 weight = gets.chomp.to_i
-formula = v.to_i - 110
+formula = weight.to_i - 110
 puts " #{name}, ваш идеальный вес #{formula}кг"
